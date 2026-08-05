f82d0071ab0e190b8d50ff9227fc32b9dd8b2788
DM001-mm <Xuhaolin2918782608@outlook.com>
2026-08-05 20:12:48 +0800

    问题基本解决

--- main.cpp
+++ main.cpp
@@ -7,12 +7,24 @@ constexpr TGAColor red     = {  0,   0, 255, 255};
 constexpr TGAColor blue    = {255, 128,  64, 255};
 constexpr TGAColor yellow  = {  0, 200, 255, 255};
 
-void drawline(int ax,int ay,int bx,int by,TGAImage& image,TGAColor color){
-    for(double t=0.0;t<1;t+=.01){
-        int x = std::round(ax+(bx-ax)*t);
-        int y = std::round(ay+(by-ay)*t);
-        image.set(x,y,color); // 设置颜色
-    }    
+void line(int ax, int ay, int bx, int by, TGAImage &framebuffer, TGAColor color) {
+    bool steep = std::abs(ax-bx) < std::abs(ay-by);
+    if (steep) { // if the line is steep, we transpose the image
+        std::swap(ax, ay);
+        std::swap(bx, by);
+    }
+    if (ax>bx) { // make it left−to−right
+        std::swap(ax, bx);
+        std::swap(ay, by);
+    }
+    for (int x=ax; x<=bx; x++) {
+        float t = (x-ax) / static_cast<float>(bx-ax);
+        int y = std::round( ay + (by-ay)*t );
+        if (steep) // if transposed, de−transpose
+            framebuffer.set(y, x, color);
+        else
+            framebuffer.set(x, y, color);
+    }
 }
 
 int main(int argc, char** argv) {
@@ -24,15 +36,15 @@ int main(int argc, char** argv) {
     int bx = 12, by = 37;
     int cx = 62, cy = 53;
 
-    // framebuffer.set(ax, ay, white); //给TGA上面的点 设置颜色
-    // framebuffer.set(bx, by, white); 
-    // framebuffer.set(cx, cy, white);
-    
-    drawline(ax, ay, bx,by,framebuffer,white);  //给TGA上面的点 设置颜色
-    drawline(bx, by, cx,cy,framebuffer,white); 
-    drawline(cx, cy,  ax,ay,framebuffer,white);
+    line(ax, ay, bx, by, framebuffer, blue);
+    line(cx, cy, bx, by, framebuffer, green);
+    line(cx, cy, ax, ay, framebuffer, yellow);
+    line(ax, ay, cx, cy, framebuffer, red);
+
+    framebuffer.set(ax, ay, white);
+    framebuffer.set(bx, by, white);
+    framebuffer.set(cx, cy, white);
 
     framebuffer.write_tga_file("framebuffer.tga");
     return 0;
 }
-
